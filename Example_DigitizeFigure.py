fb45e853208c9e2b0824f17125e24ddec94c7e4b
Tore Aadland <Tore.Aadland@uib.no>
2016-03-09 15:09:27 +0100

    Improved comments on the example file

--- Example_DigitizeFigure.py
+++ Example_DigitizeFigure.py
@@ -6,5 +6,14 @@ path = "./simpleTest.svg"
 crossSection = LoadCrossSection(path)
 DrawModel(crossSection)
 
+# To see what facies are stored:
 print(crossSection.Facies)
+
+# To see what surfaces are stored:
 print(crossSection.Surfaces)
+# This crossSection.Surfaces returns a dictionary with surface label as key and returns an object of "Surface"-class. 
+# See surfaces.py to see how this class works. 
+# Surface.Line returns a Linestring or multilinestring object.
+
+# To store the coordinates in a surface as a list;
+Coordinates = list(crossSection.Surfaces['Topography'].Line.coords)
